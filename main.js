6cf7776f72104b03a49d25dfae8c9f3c717cbf0b
Gian-Marco Hutter <32538484+gianmarcohutter@users.noreply.github.com>
2024-10-27 01:25:54 +0200

    frikkin nice

--- main.js
+++ main.js
@@ -1,16 +1,16 @@
 console.log('Script loaded - version 7');
 var apiKey = 'csek6s9r01qrf7qi1u6gcsek6s9r01qrf7qi1u70';
 var stocks = {
-    'AAPL': { price: 0, shares: 1 },
-    'MSFT': { price: 0, shares: 1 },
-    'GOOG': { price: 0, shares: 1 },
-    'AMZN': { price: 0, shares: 1 },
-    'META': { price: 0, shares: 1 },
-    'NVDA': { price: 0, shares: 1 },
-    'ORCL': { price: 0, shares: 1 },
-    'IBM': { price: 0, shares: 1 },
-    'CRM': { price: 0, shares: 1 },
-    'INTC': { price: 0, shares: 1 }
+    'AAPL': { price: 0, shares: 0 },
+    'MSFT': { price: 0, shares: 0 },
+    'GOOG': { price: 0, shares: 2.99419126894 },
+    'AMZN': { price: 0, shares: 0 },
+    'META': { price: 0, shares: 0 },
+    'NVDA': { price: 0, shares: 0 },
+    'ORCL': { price: 0, shares: 0 },
+    'IBM': { price: 0, shares: 0},
+    'CRM': { price: 0, shares: 0 },
+    'INTC': { price: 0, shares: 0 }
 };
 function init() {
     console.log('Welcomeeeeeee to Limit Party');
@@ -55,7 +55,7 @@ function calculateTotalValue() {
     }
     var totalValueElement = document.getElementById('total-value');
     if (totalValueElement) {
-        totalValueElement.textContent = "Total Portfolio Value: $".concat(totalValue.toFixed(2));
+        totalValueElement.textContent = "Rösl zum bruuche: ".concat(totalValue.toFixed(2));
     }
 }
 // This function can be called from the console to update shares
